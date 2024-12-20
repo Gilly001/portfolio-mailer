7fb465498c7f5fe2c6a60d0c877744dd08fa850d
GillyGc9 <gilbertmaina001@gmail.com>
2024-12-20 12:01:32 +0300

    cors policy

--- server.js
+++ server.js
@@ -3,7 +3,19 @@ const cors = require('cors');
 const nodemailer = require('nodemailer');
 
 const app = express();
-app.use(cors());
+const allowedOrigins = ['https://portfolio-delta-nine-63.vercel.app'];
+app.use(cors({
+    origin: function(origin, callback) {
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) {
+            var msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    },
+    methods: ['GET', 'POST', 'OPTIONS'],
+    credentials: true
+}));
 app.use(express.json());
 
 
