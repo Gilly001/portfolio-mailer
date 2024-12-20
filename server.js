bcda5f6eb192871bafdbc3ee66299135ace1fa30
GillyGc9 <gilbertmaina001@gmail.com>
2024-12-20 12:24:40 +0300

    allow cors origin

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const cors = require('cors');
+const nodemailer = require('nodemailer');
+
+const app = express();
+
+app.use(cors({
+    origin: 'https://portfolio-delta-nine-63.vercel.app'
+}));
+app.use(express.json());
+
+
+const transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false,
+    auth: {
+        user: 'gilbertmaina001@gmail.com',
+        pass: 'ylcp hcqs iblr xtka'
+    }
+});
+
+app.post('/send-email', async (req, res) => {
+    const { name, email, message } = req.body;
+
+    const mailOptions = {
+        from: email,
+        to: 'gilbertmaina001@gmail.com',
+        subject: `New portfolio mail from ${name}`,
+        text: message,
+        replyTo: email
+    };
+
+    try {
+        await transporter.sendMail(mailOptions);
+        res.status(200).send('Email sent successfully');
+    } catch (error) {
+        console.error('Error sending email:', error);
+        res.status(500).send('Error sending email');
+    }
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
