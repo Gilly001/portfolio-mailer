baeb1eaae59294cbd33fb361d4558633ce42eb93
GillyGc9 <gilbertmaina001@gmail.com>
2025-01-02 14:22:17 +0300

    configure to railway

--- api/send-email.js
+++ api/send-email.js
@@ -1,4 +1,3 @@
-// api/send-email.js
 const nodemailer = require('nodemailer');
 
 // Define the transporter outside the handler to reuse it across multiple requests
@@ -7,20 +6,20 @@ const transporter = nodemailer.createTransport({
     port: 587,
     secure: false,
     auth: {
-        user: 'gilbertmaina001@gmail.com',
-        pass: 'ylcp hcqs iblr xtka', // You should store sensitive info like this in environment variables
+        user: 'gilbertmaina001@gmail.com', // Use environment variables for sensitive data
+        pass: 'ylcp hcqs iblr xtka',
     },
 });
 
 // This is the serverless function handler
 module.exports = async (req, res) => {
-
+    // Set CORS headers
     res.setHeader('Access-Control-Allow-Credentials', true);
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS, PATCH, DELETE, POST, PUT');
     res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
 
-    // Handle OPTIONS request
+    // Handle OPTIONS request (CORS preflight)
     if (req.method === 'OPTIONS') {
         res.status(200).end();
         return;
@@ -48,4 +47,4 @@ module.exports = async (req, res) => {
         // Handle methods other than POST (e.g., GET)
         res.status(405).json({ error: 'Method Not Allowed' });
     }
-}
+};
