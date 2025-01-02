b369b47f5a62e1841ef9aa854e854c1aad3355d4
GillyGc9 <gilbertmaina001@gmail.com>
2025-01-02 14:58:11 +0300

    configure server 3

--- api/send-email.js
+++ api/send-email.js
@@ -37,5 +37,5 @@ app.post('/send-email', async (req, res) => {
     }
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
