4059e9c249ff6db4bf5f62c92e99429a941b2987
GillyGc9 <gilbertmaina001@gmail.com>
2025-01-02 13:00:43 +0300

    make this serverless

---
+++ api/send-email.js
@@ -0,0 +1,39 @@
+// api/send-email.js
+import nodemailer from 'nodemailer';
+
+// Define the transporter outside the handler to reuse it across multiple requests
+const transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false,
+    auth: {
+        user: 'gilbertmaina001@gmail.com',
+        pass: 'ylcp hcqs iblr xtka', // You should store sensitive info like this in environment variables
+    },
+});
+
+// This is the serverless function handler
+export default async function handler(req, res) {
+    if (req.method === 'POST') {
+        const { name, email, message } = req.body;
+
+        const mailOptions = {
+            from: email,
+            to: 'gilbertmaina001@gmail.com',
+            subject: `New portfolio mail from ${name}`,
+            text: message,
+            replyTo: email,
+        };
+
+        try {
+            await transporter.sendMail(mailOptions);
+            res.status(200).json({ message: 'Email sent successfully' });
+        } catch (error) {
+            console.error('Error sending email:', error);
+            res.status(500).json({ error: 'Error sending email' });
+        }
+    } else {
+        // Handle methods other than POST (e.g., GET)
+        res.status(405).json({ error: 'Method Not Allowed' });
+    }
+}
