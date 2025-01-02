46de1535d1083b87ade72d967b658a85481ec310
GillyGc9 <gilbertmaina001@gmail.com>
2025-01-02 13:54:11 +0300

    cahnge vercel json

--- api/send-email.js
+++ api/send-email.js
@@ -14,6 +14,18 @@ const transporter = nodemailer.createTransport({
 
 // This is the serverless function handler
 module.exports = async (req, res) => {
+
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+    res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
+
+    // Handle OPTIONS request
+    if (req.method === 'OPTIONS') {
+        res.status(200).end();
+        return;
+    }
+
     if (req.method === 'POST') {
         const { name, email, message } = req.body;
 
