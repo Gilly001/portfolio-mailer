a36fb145b75660b41312d46c4801f73cdea8fb79
GillyGc9 <gilbertmaina001@gmail.com>
2025-01-03 09:53:52 +0300

    hide pass and email

--- api/send-email.js
+++ api/send-email.js
@@ -12,8 +12,8 @@ const transporter = nodemailer.createTransport({
     port: 587,
     secure: false, // Use TLS
     auth: {
-        user: 'gilbertmaina001@gmail.com',
-        pass: 'ylcp hcqs iblr xtka',
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
     }
 });
 
